e9d6e000cda62ae0afda22f9d4d2e19d4013e36c
anhtv-2103 <tranvietanh2k@gmail.com>
2021-12-24 12:14:11 +0700

    RESTful API

--- MISA.Fresher.Web11/Model/Customer.cs
+++ MISA.Fresher.Web11/Model/Customer.cs
@@ -2,7 +2,9 @@
 {
     public class Customer
     {
+        public Guid CustomerId { get; set; }
         public string CustomerCode { get; set; }
         public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
